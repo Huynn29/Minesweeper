a8f13a7c99b16221ddf7f00290325aa1df52ab49
Huynn29 <23020611@vnu.edu.vn>
2024-05-23 10:35:02 +0700

    Initial commit

---
+++ constant/constant.h
@@ -0,0 +1,11 @@
+#ifndef SCREEN_WIDTH
+#define SCREEN_WIDTH 889
+#endif // SCREEN_WIDTH
+
+#ifndef SCREEN_HEIGHT
+#define SCREEN_HEIGHT 500
+#endif // SCREEN_HEIGHT
+
+#ifndef TILE_SIZE
+#define TILE_SIZE 28
+#endif // TILE_SIZE
